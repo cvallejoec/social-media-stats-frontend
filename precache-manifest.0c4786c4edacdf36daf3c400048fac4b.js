2f8c95166416d95c2a809323a435351689276c40
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-15 19:41:49 -0500

    Updates

---
+++ precache-manifest.0c4786c4edacdf36daf3c400048fac4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf97f132c25b5d24973e42e19f7d295d",
+    "url": "/social-media-stats-frontend/index.html"
+  },
+  {
+    "revision": "85452476728acb743aa3",
+    "url": "/social-media-stats-frontend/static/css/main.29523803.chunk.css"
+  },
+  {
+    "revision": "3c462b77979b98a3a608",
+    "url": "/social-media-stats-frontend/static/js/2.7ab02e2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-stats-frontend/static/js/2.7ab02e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85452476728acb743aa3",
+    "url": "/social-media-stats-frontend/static/js/main.3913e5b6.chunk.js"
+  },
+  {
+    "revision": "5b2c40228cb0fe2a0056",
+    "url": "/social-media-stats-frontend/static/js/runtime-main.778fe687.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-stats-frontend/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-stats-frontend/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-stats-frontend/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-stats-frontend/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-stats-frontend/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-stats-frontend/static/media/icon-youtube.8499da12.svg"
+  }
+]);
